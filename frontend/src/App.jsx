fe815b9b783e0bd217c37d3f76b81adccbc5a160
dante266 <dquinta1@swarthmore.edu>
2021-11-29 00:14:40 -0500

    updating App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,12 +14,12 @@ function App() {
 			<QueryClientProvider client={queryClient}>
 				<Router>
 					<Routes>
-						<Route path='/login' element={LandingPage} />
-						<Route path='/signup' element={SignUpForm} />
+						<Route path='/login' element={<LandingPage />} />
+						<Route path='/signup' element={<SignUpForm />} />
 
 						{/* These should be protected routes */}
-						<Route path='/' element={MainPage} />
-						{/* <Route path='/profile' element={ProfilePage} /> */}
+						<Route path='/' element={<MainPage />} />
+						{/* <Route path='/profile' element={<ProfilePage />} /> */}
 						<Route path='*' element={() => '404 NOT FOUND'} />
 					</Routes>
 				</Router>
